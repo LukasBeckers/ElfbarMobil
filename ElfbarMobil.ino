ed9cb654469d23d20b87e210e07e53b6e9c54c8d
LukasBeckers <lukas.bckrs@gmai.com>
2024-06-13 23:04:37 +0200

    build: Changes pwm mapping from potentiometer to thumb throttle

--- ElfbarMobil.ino
+++ ElfbarMobil.ino
@@ -1,10 +1,14 @@
 const int acceleratorPin = A3;
+const int digitalAcceleratorPin = 3; // For testing, maybe the thumbthrottle sends a pwm signal, who knows? 
 const int motorPin = 5;
-const int mosfetPowerPin; // Used to turn on, of the Mosfets
+const int mosfetPowerPin = 8; // Used to turn on, of the Mosfets
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(acceleratorPin, OUTPUT);
+  pinMode(acceleratorPin, INPUT);
+
+  // start the Mosfet driver
+  digitalWrite(mosfetPowerPin, HIGH);
 
   Serial.begin(9600);
 }
@@ -13,9 +17,11 @@ void loop() {
   // put your main code here, to run repeatedly:
   int acceleratorRead = analogRead(acceleratorPin);
 
-  // convert to value between 255 and zero for 8 bit pwm input.
-  int pwmValue = map(acceleratorRead, 0, 1023, 0, 255);
 
+  // convert to value between 255 and zero for 8 bit pwm input.
+  int pwmValue = constrain(map(acceleratorRead, 180, 850, 0, 255), 0, 255);
+
+  // adapt the pwd signal.
   analogWrite(motorPin, pwmValue);
 
   Serial.print("Potentiometer Value: ");
